f24e9c58deec98d4ba8273a415be7ea0aecf6368
Kelvin Bush <kelybush@gmail.com>
2022-08-09 14:38:34 +0300

    add indexes to foreign keys

--- db/migrate/20220809103023_add_references.rb
+++ db/migrate/20220809103023_add_references.rb
@@ -1,7 +1,7 @@
 class AddReferences < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :author,  foreign_key: {to_table: :users}
-    add_reference :comments, :author,  foreign_key: {to_table: :users}
-    add_reference :likes, :author,  foreign_key: {to_table: :users}
+    add_reference :posts, :author, foreign_key: { to_table: :users }, index: true
+    add_reference :comments, :author, foreign_key: { to_table: :users }, index: true
+    add_reference :likes, :author, foreign_key: { to_table: :users }, index: true
   end
 end
