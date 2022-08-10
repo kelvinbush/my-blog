4223ee1d8189c0f1dd60d248300e0d4c6570723f
Kelvin Bush <kelybush@gmail.com>
2022-08-10 08:54:14 +0300

    fix numericality

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,8 @@ class Post < ApplicationRecord
   after_save :update_posts_counter
 
   validates :title, presence: true, length: { in: 4...250 }
-  validates :comments_counter, numericality: { only_integer: true, minimum: 0 }
-  validates :likes_counter, numericality: { only_integer: true, minimum: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     author.update(posts_counter: author.posts.count)
