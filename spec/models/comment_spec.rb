c24531bc31dd53fc89f4cf4516620c540b58bc8c
Kelvin Bush <kelybush@gmail.com>
2022-08-11 04:56:44 +0300

    test user

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  first_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+  first_post = Post.new(author: first_user, title: 'Hello', text: 'This is my first post')
+  comment = Comment.new(post: first_post, author: first_user, text: 'Hi Tom!')
+  comment2 = Comment.new(post: first_post, author: first_user, text: 'Hi John!')
+  comment3 = Comment.new(post: first_post, author: first_user, text: 'Hi Mike!')
+
+  before(:all) do
+    first_user.save
+    first_post.save
+    comment.save
+  end
+
+  it 'text should update posts comment counter' do
+    first_user.name = nil
+    expect(first_user).to_not be_valid
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 3) }
+
+  before { subject.save }
+
+  it 'should save the user' do
+    expect(subject).to be_valid
+  end
+
+  it 'name should be present' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should have equal to or greater than 0 post counter' do
+    subject.posts_counter = nil
+    expect(subject).to_not be_valid
+  end
+
 end
