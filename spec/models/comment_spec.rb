3968f28b193defbcd6265eb150624cc73f56ce90
Kelvin Bush <kelybush@gmail.com>
2022-08-11 05:17:23 +0300

    test comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  first_user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-  first_post = Post.new(author: first_user, title: 'Hello', text: 'This is my first post')
-  comment = Comment.new(post: first_post, author: first_user, text: 'Hi Tom!')
-  comment2 = Comment.new(post: first_post, author: first_user, text: 'Hi John!')
-  comment3 = Comment.new(post: first_post, author: first_user, text: 'Hi Mike!')
-
-  before(:all) do
-    first_user.save
-    first_post.save
-    comment.save
+  subject do
+    first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)
+    first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
+    Comment.create(post: first_post, author: first_user, text: 'Hi Tom!')
+    Comment.create(post: first_post, author: first_user, text: 'Hi John!')
+    Comment.new(post: first_post, author: first_user, text: 'Hi Mike!')
   end
 
-  it 'text should update posts comment counter' do
-    first_user.name = nil
-    expect(first_user).to_not be_valid
+  before { subject.save }
+
+  it 'should save a comment' do
+    expect(subject).to be_valid
+  end
+
+  it 'should update comments counter' do
+    expect(subject.post.comments_counter).to eq 3
   end
 
 end
