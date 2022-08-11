dd6bd757a8c0021af9cd4f2965f47b3e3f5068f8
Kelvin Bush <kelybush@gmail.com>
2022-08-11 05:42:40 +0300

    update user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 3) }
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
 
   before { subject.save }
 
@@ -19,4 +19,12 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
+  it 'should get the last 3 posts' do
+    Post.create(title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0, author: subject)
+    Post.create(title: 'Hello2', text: 'This is my second post', comments_counter: 0, likes_counter: 0, author: subject)
+    Post.create(title: 'Hello3', text: 'This is my third post', comments_counter: 0, likes_counter: 0, author: subject)
+    Post.create(title: 'Hello4', text: 'This is my fourth post', comments_counter: 0, likes_counter: 0, author: subject)
+    expect(subject.last_three_posts.length).to eq 3
+    expect(subject.last_three_posts[0].text).to eq 'This is my fourth post'
+  end
 end
