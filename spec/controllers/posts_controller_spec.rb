5745ba55d7e3637fd422e4dd9a4b3ea3981d71a0
Kelvin Bush <kelybush@gmail.com>
2022-08-16 05:35:22 +0300

    add test for get post as per post id

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
+  context 'when GET /show' do
+    before(:example) { get user_post_path(1, 1) }
+
+    it 'should have success code of 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "should render the 'show' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it 'should display correct content in the views' do
+      expect(response.body).to include('A post  based on the post id')
+    end
+  end
 end
