7740ee89fe45e5a2ccd0c3bed4540fb92618b5e9
Kelvin Bush <kelybush@gmail.com>
2022-08-18 05:45:29 +0300

    set up new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'comment/new'
+  get 'comment/create'
   root to: 'users#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:index, :create]
+      resources :likes, only: [:create]
+    end
   end
 
 end
