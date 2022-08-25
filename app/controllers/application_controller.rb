6c6aa7d13bf2d0adf7b0a83c0e1d5bd2fe18db95
Kelvin Bush <kelybush@gmail.com>
2022-08-25 20:27:24 +0300

    configure cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
+  respond_to :json, :html
+  protect_from_forgery unless: -> { request.format.json? }
   before_action :authenticate_user!
 
   before_action :update_allowed_parameters, if: :devise_controller?
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_back fallback_location: root_path, notice: exception.message }
+      format.js { head :forbidden, content_type: 'text/html' }
+    end
+  end
 
   protected
 
