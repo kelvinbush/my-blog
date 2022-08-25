35d3fe193cc4d021c06fe938d33138d1ec4f8f2c
Kelvin Bush <kelybush@gmail.com>
2022-08-25 13:08:31 +0300

    set up cancancan in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+
   before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,22 @@
 class CommentsController < ApplicationController
+
   def create
     post = Post.find(params[:post_id])
     @comment = Comment.create(comment_params)
     @comment.post = post
-    @comment.author = Current.user
+    @comment.author = current_user
     if @comment.save
       flash[:notice] = 'Comment added successfully.'
       redirect_to user_post_path(post.author, post)
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to user_post_path(current_user, params[:post_id]), notice: 'Successfully removed the comment.'
   end
 
   private
